99d3c717c9738f0c31987da7ce874fb4b10041d4
gard-en <gard.en@outlook.com>
2023-10-12 12:12:27 +1030

    math operations + vector3 struct

---
+++ MathU/Operations.cs
@@ -0,0 +1,44 @@
+﻿public abstract class Operations
+{
+	float Sqrt(float number)
+	{
+		return 0;
+	}
+
+	public static float Floor(float x)
+	{
+		return (float)(int)x;
+	}
+
+	public static double Square(double x)
+	{
+		return (x * x);
+	}
+
+	public static double Sqrt(double x)
+	{
+		double y = 0;
+
+		int p, square, c;
+
+		p = 0;
+		do
+		{
+			p++;
+			square = (p + 1) * (p + 1);
+		}
+		while (x > square);
+
+		y = (double)p;
+		c = 0;
+
+		while (c < 10)
+		{
+			y = (x / y + y) / 2;
+			if (y * y == x)
+				return y;
+			c++;
+		}
+		return y;
+	}
+}
--- MathU/Vector3.cs
+++ MathU/Vector3.cs
@@ -1,14 +1,18 @@
-﻿namespace MathU
+﻿public struct Vector3
 {
-	public struct Vector3
-	{
-		float x, y, z;
+	public float x;
+	public float y;
+	public float z;
 
-		public Vector3(float _x, float _y, float _z)
-		{
-			x = _x; 
-			y = _y; 
-			z = _z; 
-		}
+	public Vector3(float _x, float _y, float _z)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+
+	public float Magnitude()
+	{
+		return 0;
 	}
 }
