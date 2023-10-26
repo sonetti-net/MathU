e06459b13f65bf0eae73cdde8f5996675dca89df
gard-en <gard.en@outlook.com>
2023-10-26 19:21:16 +1030

    Matrix class + multiplication.

---
+++ MathU/Matrix.cs
@@ -0,0 +1,58 @@
+﻿namespace MathU
+{
+	public class Matrix
+	{
+		public int rows { get; set; }
+		public int cols { get; set; }
+
+		public int[,] data { get; set; }
+
+		public Matrix(int rows, int cols)
+		{
+			this.rows = rows;
+			this.cols = cols;
+			this.data = new int[rows, cols];
+		}
+
+		public override string ToString()
+		{
+			string output = string.Empty;
+
+			for (int x = 0; x < this.rows; x++)
+			{
+				for (int y = 0; y < this.cols; y++)
+				{
+					output += $"{this.data[x, y]}, ";
+				}
+				output += "\n";
+			}
+
+			return output;
+		}
+		public static Matrix operator *(Matrix A, Matrix B)
+		{
+			if (A.cols == B.rows)
+			{
+				Matrix result = new Matrix(A.rows, B.cols);
+				for (int i = 0; i < A.rows; i++)
+				{
+					for ( int j = 0; j < B.cols; j++)
+					{
+						int sum = 0;
+						for (int k = 0; k < A.cols; k++)
+						{
+							sum += A.data[i,k] * B.data[k, j];
+						}
+						result.data[i, j] = sum;
+					}
+				}
+
+				return result;
+			}
+			else
+			{
+				throw new Exception("Cannot multiply");
+			}
+		}
+	}
+}
--- MathU/Vector3.cs
+++ MathU/Vector3.cs
@@ -1,19 +1,64 @@
-﻿public struct Vector3
+﻿namespace MathU
 {
-	public float x;
-	public float y;
-	public float z;
-
-	public Vector3(float _x, float _y, float _z)
+	public struct Vector3
 	{
-		x = _x;
-		y = _y;
-		z = _z;
-	}
+		public float x;
+		public float y;
+		public float z;
 
-	public float Magnitude()
-	{
-		
-		return 0;
+		public Vector3(float _x, float _y, float _z)
+		{
+			x = _x;
+			y = _y;
+			z = _z;
+		}
+
+		public static Vector3 operator +(Vector3 a, Vector3 b)
+		{
+			return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+		}
+
+		public static Vector3 operator -(Vector3 a, Vector3 b)
+		{
+			return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+		}
+
+		public static Vector3 operator /(Vector3 a, float divisor)
+		{
+			if (divisor == 0)
+			{
+				return new Vector3(0, 0, 0);
+			}
+			return new Vector3(a.x / divisor, a.y / divisor, a.z / divisor);
+		}
+
+		public static Vector3 operator *(Vector3 a, float scalar)
+		{
+			return new Vector3(a.x * scalar, a.y * scalar, a.z * scalar);
+		}
+
+		public float Magnitude()
+		{
+			return (float)Operations.Sqrt(Operations.Square(x) + Operations.Square(y) + Operations.Square(z));
+		}
+
+		public static Vector3 Distance(Vector3 Origin, Vector3 Destination)
+		{
+			return new Vector3(Destination.x - Origin.x, Destination.y - Origin.y, Destination.z - Origin.z);
+		}
+
+		public Vector3 Normalized()
+		{
+			return new Vector3(this.x / this.Magnitude(), this.y / this.Magnitude(), this.z / this.Magnitude());
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns>String representation of Vector3 x y z</returns>
+		public override string ToString()
+		{
+			return new string($"{x} {y} {z}");
+		}
 	}
 }
