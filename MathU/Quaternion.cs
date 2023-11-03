1d600fa28366ba027f453c2ff9965831c754cf05
Sonett-i <sam@sonetti.net>
2023-11-03 12:54:26 +1030

    Quaternion

---
+++ MathU/Quaternion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathU
+{
+	public struct Quaternion
+	{
+		public float i, j, k, w;
+
+		public Quaternion(float i, float j, float k, float w)
+		{
+			
+			this.i = i;
+			this.j = j;
+			this.k = k;
+			this.w = w;
+		}
+
+		public Quaternion(float value)
+		{
+
+			this.i = value;
+			this.j = value;
+			this.k = value;
+			this.w = value;
+		}
+		//deep
+		public override string ToString()
+		{
+			return $"({i}, {j}, {k}, {w}";
+		}
+
+		public static Quaternion operator +(Quaternion left, Quaternion right)
+		{
+			Quaternion result = new Quaternion(0);
+
+			result.i = left.i + right.i;
+			result.j = left.j + right.j;
+			result.k = left.k + right.k;
+			result.w = left.w + right.w;
+
+			return result;
+		}
+	}
+}
